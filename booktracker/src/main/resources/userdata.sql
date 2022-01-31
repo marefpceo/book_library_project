5c605006735c5da1b2d748129f074b091faf60b2
Trupti Thakur <53278969+Trupti-GitRepository@users.noreply.github.com>
2022-01-31 00:11:32 -0500

    Delete booktracker directory

--- booktracker/src/main/resources/userdata.sql
+++
@@ -1,5 +0,0 @@
-insert into user_login (id, firstName, lastName, username, password, enabled, expired, rec_lgcl_del_ind) values(1, 'Samuel', 'Mathue', 'smathue007@gmail.com','Sateri@202112', 1, 1, 'N');
-
-insert into user_auth (id, authority) values(1, 'write');
-
-insert into user_info (id, fav_genres, avg_reading_hrs_daily) values(1, 'Sci-Fi', 2);
