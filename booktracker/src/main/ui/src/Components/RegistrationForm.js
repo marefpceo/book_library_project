3b140ea43c2057b2412e15ca3200f9e41401cbaf
Micah M. Tinife <micahking2k@gmail.com>
2022-02-20 01:37:53 -0500

    Styled Messages
    Styled register error and success messages

--- booktracker/src/main/ui/src/Components/RegistrationForm.js
+++ booktracker/src/main/ui/src/Components/RegistrationForm.js
@@ -126,22 +126,60 @@ const RegistrationForm = () => {
     <>
     {
       success ? (
-        <section className="w-128 flex justify-center flex-col self-center shadow-md bg-white rounded px-8 pt-6 pb-8 mb-4 font-['Roboto]">
-          <h1 className="text-center text-4xl text-lime-500 mb-10">Registration Successful!</h1>
-          <p className="flex justify-center">
-          <Link
+        // <section className="w-128 flex justify-center flex-col self-center shadow-md bg-white rounded px-8 pt-6 pb-8 mb-4 font-['Roboto]">
+        //   <h1 className="text-center text-4xl text-lime-500 mb-10">Registration Successful!</h1>
+        //   <p className="flex justify-center">
+        //   <Link
+        //     to="/login"
+        //     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        //   >
+        //     Login
+        //   </Link>
+        //   </p>
+        // </section>
+        <section
+          className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white"
+        >
+          <div className="mt-3 flex flex-col text-center">
+            <div
+              className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100"
+            >
+              <svg
+                className="h-6 w-6 text-green-600"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 13l4 4L19 7"
+                ></path>
+              </svg>
+            </div>
+            <h3 className="text-lg leading-6 font-medium text-gray-900">Successful!</h3>
+            <div className="mt-2 px-7 py-3">
+              <p className="text-sm text-gray-500">
+                Account has been successfully registered!
+              </p>
+            </div>
+            <div className="items-center px-4 py-3">
+            <Link
             to="/login"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           >
             Login
           </Link>
-          </p>
+            </div>
+          </div>
         </section>
       ) : (
     <section>
       <p
         ref={errRef}
-        className={errMsg ? "errmsg" : "offscreen"}
+        className={errMsg ? "bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" : "offscreen"}
         aria-live="assertive"
       >
         {errMsg}
