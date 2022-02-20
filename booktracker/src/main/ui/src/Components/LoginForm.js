ae32885d6f7cb76168166708403842192bc6df2c
Micah M. Tinife <micahking2k@gmail.com>
2022-02-20 00:26:10 -0500

    Added Front-end authentication
    Set up login page with authentication

--- booktracker/src/main/ui/src/Components/LoginForm.js
+++ booktracker/src/main/ui/src/Components/LoginForm.js
@@ -1,54 +1,74 @@
-import React from 'react'
-import { Link } from "react-router-dom"
-import { Formik } from 'formik'
-import TextField from './TextField'
-import * as Yup from 'yup'
+import { useRef, useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom'
 
-function LoginForm() {
-  const validate = Yup.object({
-    email: Yup.string()
-      .email('Email is invalid!')
-      .required('Email is required'),
-    password: Yup.string()
-      .min(6, 'Password must be at least 6 characters')
-      .required('Password is required'),
-  });
-  return <Formik
-    initialValues={{
-      email: '',
-      password: '',
-    }}
-    validationSchema={validate}
-    onSubmit={values => {
-      console.log(values)
-    }}
-  >
-    {formik => (
-      <div>
-        <h1 className="text-center text-4xl text-gray-500 mb-10">Login</h1>
-        <form action="" className="w-96 flex justify-center flex-col self-center shadow-md bg-white rounded px-8 pt-6 pb-8 mb-4 font-['Roboto]">
-          <div className="mb-3">
-            <TextField type="email"
-              name="email"
-              label="Email Address"
-            />
-          </div>
-          <div className="mb-3">
-            <TextField type="password"
-              name="password"
-              label="Password"
-            />
-          </div>
-          <div className="flex items-center">
-            <button className="text-white w-full rounded-full bg-blue-500 hover:bg-blue-700:text-white font-bold py-2 px-4 round">Login</button>
-          </div>
-          <div className="mt-14 text-center">
-            Are you new? <Link to="/register" className="text-bold text-blue-500">Register</Link>
-          </div>
-        </form>
-      </div>
-    )}
-  </Formik>
+const LoginForm = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, [])
+
+  useEffect(() => {
+    setErrMsg('');
+  }, [user, pwd])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+
+  }
+
+  return (
+    <section>
+      <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+      <h1 className="text-center text-4xl text-gray-500 mb-10">Sign In</h1>
+      <form onSubmit={handleSubmit} className="w-128 flex justify-center flex-col self-center shadow-md bg-white rounded px-8 pt-6 pb-8 mb-4 font-['Roboto]">
+        <label htmlFor="username" className="block text-gray-700 text-sm mb-2">Username</label>
+        <input 
+          type="text" 
+          id="username"
+          ref={userRef}
+          autoComplete="off"
+          onChange={(e) => setUser(e.target.value)}
+          value={user}
+          required
+          className={
+            "w-full shadow appearance-none border rounded py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder-red-700 text-base rounded-lg "
+          }
+        />
+        <label htmlFor="password" className="block text-gray-700 text-sm mb-2">Password</label>
+        <input 
+          type="password" 
+          id="password"
+          onChange={(e) => setPwd(e.target.value)}
+          value={pwd}
+          required
+          className={
+            "w-full shadow appearance-none border rounded py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder-red-700 text-base rounded-lg "
+          }
+        />
+        <button className="mt-6 text-white w-full rounded-full bg-blue-500 hover:bg-blue-700:text-white font-bold py-2 px-4 round">Sign In</button>
+      </form>
+      <p className="flex mt-14 justify-center">
+        Need an Account?
+        <br />
+        <span className="pl-2">
+          <Link
+            to="/register"
+            className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-300 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-gray-900 dark:hover:bg-gray-700 dark:hover:text-gray-300 md:dark:hover:bg-transparent dark:border-gray-700"
+          >
+            Register
+          </Link>
+        </span>
+      </p>
+    </section>
+  )
 }
 
-export default LoginForm;
+export default LoginForm
--- booktracker/src/main/ui/src/index.js
+++ booktracker/src/main/ui/src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './index.css'
 import App from './App'
+import { AuthProvider } from './context/AuthProvider'
 import Login from './Pages/Login/login'
 import Registration from './Pages/Registration/registration'
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="login" element={<Login />} />
-      <Route path="register" element={<Registration />} />
-    </Routes>
-  </BrowserRouter>,
+  <AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Registration />} />
+      </Routes>
+    </BrowserRouter>
+  </AuthProvider>,
   document.getElementById('root')
 );
